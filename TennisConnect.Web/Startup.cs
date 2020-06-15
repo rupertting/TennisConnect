6f3dad9023d546c0125e1c7f4d156288d78c41a7
Rupert Ting <rupert.zf.ong@gmail.com>
2020-06-15 19:09:40 +0100

    Registered UserService layer in container

--- TennisConnect.Web/Startup.cs
+++ TennisConnect.Web/Startup.cs
@@ -76,6 +76,8 @@ namespace TennisConnect.Web
                     ValidateAudience = false
                 };
             });
+
+            services.AddScoped<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
