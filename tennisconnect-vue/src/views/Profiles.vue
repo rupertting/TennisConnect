bfa4805f36176d8ce1653e97e59b87dd5cd520c7
Rupert Ting <rupert.zf.ong@gmail.com>
2020-07-03 10:45:21 +0100

    ProfileListItem nested component

--- tennisconnect-vue/src/views/Profiles.vue
+++ tennisconnect-vue/src/views/Profiles.vue
@@ -3,54 +3,45 @@
     <h1>Profiles</h1>
     <div v-if="profiles.items">
       <div v-for="profile in profiles.items" :key="profile.id">
-       <router-link :to="{ name: 'Profile',
-                           params: { id: profile.id } 
-                      }">
-         <div class="profile-wrapper">
-          <div>
-            <strong>Name</strong>: {{ profile.userModel.firstName }} {{ profile.userModel.lastName }} 
-          </div>
-          <div v-if="profile.club !== null">
-            <strong>Club</strong>: {{ profile.club.name }}
-          </div>
-          <div v-if="profile.address !== null" >
-            <strong>Location</strong>: {{ profile.address.town }}
-          </div>
-        </div>
-       </router-link>
+        <router-link :to="{ name: 'Profile', params: { id: profile.id } }">
+          <ProfileListItem :profile="profile"> </ProfileListItem>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {mapState, mapActions} from 'vuex';
-import { Component, Vue} from "vue-property-decorator";
+import { mapState, mapActions } from "vuex";
+import { Component, Vue } from "vue-property-decorator";
+import ProfileListItem from "@/components/ProfileListItem.vue";
 
-export default{
-  name: 'Profiles',
-  components:{},
+export default {
+  name: "Profiles",
+  components: {
+    ProfileListItem,
+  },
   computed: {
     ...mapState({
-      profiles: state => state.profiles.all
-    })
+      profiles: (state) => state.profiles.all,
+    }),
   },
-  created(){
+  created() {
     this.getAllProfiles();
   },
   methods: {
-    ...mapActions('profiles', {
-      getAllProfiles: 'getAll'
-    })
-  }
-}
+    ...mapActions("profiles", {
+      getAllProfiles: "getAll",
+    }),
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .profile-wrapper {
-    margin: 0.8rem;
-    padding: 0.4rem;
-    border: 1px solid #555;
-    border-radius: 1rem;
-  }
+.profile-wrapper {
+  margin: 0.8rem;
+  padding: 0.4rem;
+  border: 1px solid #555;
+  border-radius: 1rem;
+}
 </style>
